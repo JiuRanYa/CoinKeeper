76c7133585c0354ae691f2906701c6cb2af4d893
1084350607 <1084350607@qq.com>
2023-08-18 15:34:49 +0800

    feat: add home page

---
+++ lib/ui/views/homePage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('123'),
+    );
+  }
+}
