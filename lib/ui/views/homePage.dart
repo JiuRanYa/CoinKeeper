fb0b0ae1e8e7c321ac7ad68fb48a5e4603e1ee7a
1084350607 <1084350607@qq.com>
2023-08-18 17:24:01 +0800

    chore: svg deps & bottom logo

--- lib/ui/views/homePage.dart
+++ lib/ui/views/homePage.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
   _HomePageState createState() => _HomePageState();
+}
+
+class CustomStyle<T> {
+  T? height;
+  T? width;
+
+  CustomStyle(this.height, this.width);
 }
 
 class _HomePageState extends State<HomePage> {
@@ -21,10 +29,14 @@ class _HomePageState extends State<HomePage> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              _buildBottomBarItem('流水'),
-              _buildBottomBarItem('流水'),
-              _buildBottomBarItem('流水'),
-              _buildBottomBarItem('流水'),
+              _buildBottomBarItem(
+                  '流水1', 'static/images/bottom1.svg', CustomStyle(26, 26)),
+              _buildBottomBarItem(
+                  '流水1', 'static/images/bottom2.svg', CustomStyle(26, 26)),
+              _buildBottomBarItem(
+                  '流水1', 'static/images/bottom3.svg', CustomStyle(26, 26)),
+              _buildBottomBarItem(
+                  '流水1', 'static/images/bottom4.svg', CustomStyle(24, 24)),
             ],
           ),
         ),
@@ -32,12 +44,18 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Column _buildBottomBarItem(String label) {
+  Column _buildBottomBarItem(
+      String label, String sourcePath, CustomStyle<double>? style) {
     return Column(
       children: [
         Container(
           margin: const EdgeInsets.only(top: 8),
-          child: Text(label),
+          child: SvgPicture.asset(
+            sourcePath,
+            height: style?.height ?? 26,
+            width: style?.width ?? 26,
+            fit: BoxFit.cover,
+          ),
         )
       ],
     );
