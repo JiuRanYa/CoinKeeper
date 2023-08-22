575efb9a5a853ec32a410f2ab84894030844e9f0
1084350607 <1084350607@qq.com>
2023-08-22 16:37:58 +0800

    feat: remove highlight effect

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:coinkeeper/ui/views/homePage.dart';
+import 'package:coinkeeper/ui/views/recordPage.dart';
+import 'package:coinkeeper/ui/views/settingPage.dart';
+import 'package:coinkeeper/ui/views/reportPage.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  _MainApp createState() => _MainApp();
+}
+
+class _MainApp extends State<MainApp> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static const List<Widget> _router = <Widget>[
+    HomePage(),
+    RecordPage(),
+    ReportPage(),
+    SettingPage(),
+  ];
+
+  BottomNavigationBarItem _buildBottomBarItem(
+      String label, String sourcePath, CustomStyle<double>? style) {
+    return BottomNavigationBarItem(
+      icon: SvgPicture.asset(
+        sourcePath,
+        height: style?.height ?? 26,
+        width: style?.width ?? 26,
+      ),
+      label: label,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomePage());
+    return MaterialApp(
+      theme: ThemeData(
+        highlightColor: Colors.transparent,
+        splashFactory: NoSplash.splashFactory,
+      ),
+      home: Scaffold(
+        body: _router.elementAt(_selectedIndex),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: <BottomNavigationBarItem>[
+            _buildBottomBarItem(
+                '流水', 'static/images/bottom1.svg', CustomStyle(26, 26)),
+            _buildBottomBarItem(
+                '流水', 'static/images/bottom2.svg', CustomStyle(26, 26)),
+            _buildBottomBarItem(
+                '流水', 'static/images/bottom3.svg', CustomStyle(26, 26)),
+            _buildBottomBarItem(
+                '设置', 'static/images/bottom4.svg', CustomStyle(24, 24)),
+          ],
+          selectedItemColor: Colors.amber[800],
+          currentIndex: _selectedIndex,
+          onTap: (int index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+        ),
+      ),
+    );
   }
 }
---
+++ lib/ui/views/recordPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecordPage extends StatefulWidget {
+  const RecordPage({super.key});
+
+  @override
+  _RecordPageState createState() => _RecordPageState();
+}
+
+class _RecordPageState extends State<RecordPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Text('Record'));
+  }
+}
