ac62320237268d2d36b68655a9eb78fc7bc67458
1084350607 <1084350607@qq.com>
2023-08-23 09:46:48 +0800

    feat: move floating action button to home page

--- lib/ui/views/homePage.dart
+++ lib/ui/views/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,38 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Text('Home'));
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        tooltip: 'Increment Counter',
+        backgroundColor: Colors.orange,
+        child: SvgPicture.asset(
+          'static/images/plus.svg',
+          height: 28,
+          width: 28,
+        ),
+      ),
+      body: SizedBox(
+        child: Row(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    '默认账单',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
